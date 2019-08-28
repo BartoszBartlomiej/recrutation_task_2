c24e2a8613fd2b688942dba5ffc4b4cb35bc55ff
BartoszBartlomiej <bartosz.lewicki@gmail.com>
2019-08-28 14:15:06 +0200

    create folder tree

---
+++ src/components/Coordinator/Coordinator.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class Coordinator extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {  }
+    }
+    render() { 
+        return ( 
+            <div>
+                
+            </div>
+         );
+    }
+}
+ 
+export default Coordinator;
