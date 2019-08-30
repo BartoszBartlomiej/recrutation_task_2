895b117453668c02722a6b03ba52ae9ab7e3ded7
BartoszBartlomiej <bartosz.lewicki@gmail.com>
2019-08-30 16:38:41 +0200

    about section in progress

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -10,13 +10,51 @@ class About extends Component {
         }
     }
 
+    handleTitleChange = (e) => {
+        this.setState({
+            title: e.target.value,
+        });
+    };
+
+    handleDescriptionChange = (e) => {
+        this.setState({
+            description: e.target.value
+        })
+    }
+
+
+
+
 
     render() {
         return (
             <div className="about box">
                 <h2>About</h2>
                 <div className="under_line" />
-                
+                <form>
+                    <div className="input_container">
+                        <p>TITLE <span>*</span></p>
+                        <input value={this.state.title} onChange={this.handleTitleChange} placeholder="Make it short and clear" />
+                    </div>
+                    <div className="input_container">
+                        <p>DESCRIPTION <span>*</span></p>
+                        <div className="textarea_container">
+                            <textarea value={this.state.description} onChange={this.handleDescriptionChange} maxLength='140' placeholder="Write about your event, be creative" />
+                            <div className="counter_container">
+                                <p>Max lenght 140 characters</p>
+                                <p>0/140</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="input_container">
+                        <p>CATEGORY</p>
+                    </div>
+                    <div className="input_container">
+                        <p>PAYMENT</p>
+                        <input type="checkbox"></input>
+                    </div>
+                </form>
+
 
 
 
